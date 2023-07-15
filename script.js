35be8d987e72fea78cbf74c7be24172e4a66aa38
adula <adulahalkano707@gmail.com>
2023-07-15 15:48:04 +0300

    modifications amde on the menu for mobile mode, pushed with windows

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+
+const restofbody = document.querySelector('.even-columns');
 const nav = document.querySelector('nav')
 const dropDown = document.querySelectorAll('.dropdown');
 const options = document.querySelectorAll('.options');
 const mobile = document.querySelectorAll('.hidden_small');
 const menu = document.querySelector('.menu_bar');
-console.log(menu)
 const hamburger = document.querySelector('.hamburger');
-const close = document.createElement('img');
-close.setAttribute('src', '../Introduction_Page/images/icon-close-menu.svg');
-close.classList = 'menu-hide'
-close.style.height = '1.3rem';
+const close = document.querySelector('.close-button');
+
 
 dropDown.forEach((button) => {
     button.addEventListener('click', () => {
@@ -22,15 +21,21 @@ dropDown.forEach((button) => {
 
 hamburger.addEventListener('click', () => {
     menu.replaceChild(close, hamburger);
+    close.classList.toggle('close-hide')
+    menu.classList.toggle('fixed');
     mobile.forEach((tab) => {
         tab.classList.remove('hidden_small');
     })
+    restofbody.classList.toggle('even-columns-extra')
 })
 
 close.addEventListener('click', () => {
     menu.replaceChild(hamburger, close);
+    close.classList.toggle('close-hide')
+    menu.classList.toggle('fixed');
     mobile.forEach((tab) => {
         tab.classList.add('hidden_small');
     })
+    restofbody.classList.toggle('even-columns-extra')
 })
 
